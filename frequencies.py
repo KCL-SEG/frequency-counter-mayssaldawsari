4ba625fb659e23f2c2db04c534f2d1a01c5724d5
Mayss Aldawsari <mayss.aldawsari@gmail.com>
2023-10-11 11:43:17 +0100

    Write code for frequencies method

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,18 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    def helper(i):
+        if i not in frequencies:
+            frequencies[i] = 1
+        else:
+            frequencies[i] += 1
+
+    for i in items:
+        if isinstance(i, str):
+            helper(i)
+        else:
+            i = str(i)
+            helper(i)
+
     return frequencies
